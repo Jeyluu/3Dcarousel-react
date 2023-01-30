8240a8d23899f9efe413246940c19e1f1b934de2
Jérémy Luu <73641462+Jeyluu@users.noreply.github.com>
2023-01-30 21:31:19 +0100

    wordking 3D carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import './App.css'
+import Carousel3D from './components/Carousel3D'
 
 function App() {
+  const slides = [
+    {
+      key: 1,
+      content: <img src="https://picsum.photos/800/800/?random" alt="1" />,
+    },
+    {
+      key: 2,
+      content: <img src="https://picsum.photos/800/800/?random" alt="2" />,
+    },
+    {
+      key: 3,
+      content: <img src="https://picsum.photos/600/800/?random" alt="3" />,
+    },
+    {
+      key: 4,
+      content: <img src="https://picsum.photos/800/500/?random" alt="4" />,
+    },
+  ]
   return (
     <div className="App">
       <header className="App-header">
         <h1>test</h1>
+        <Carousel3D card={slides} offset={200} showArrow={false} />
       </header>
     </div>
   )
---
+++ src/components/Carousel3D.tsx
@@ -0,0 +1,44 @@
+import Carousel from 'react-spring-3d-carousel'
+import { useState, useEffect } from 'react'
+import { config } from 'react-spring'
+
+interface Carousel3DP {
+  card: {
+    key: number
+    content: JSX.Element
+  }[]
+  offset: number
+  showArrow: boolean
+}
+
+function Carousel3D(props: Carousel3DP) {
+  const { card, offset, showArrow } = props
+
+  const table = card.map((element, index) => {
+    return { ...element, onClick: () => setGoToSlide(index) }
+  })
+
+  const [showArrows, setShowArrows] = useState(false)
+  const [offsetRadius, setOffsetRadius] = useState(4)
+  const [goToSlide, setGoToSlide] = useState(0)
+  const [cards] = useState(table)
+
+  useEffect(() => {
+    setOffsetRadius(offset)
+    setShowArrows(showArrow)
+  }, [offset, showArrow, showArrows])
+
+  return (
+    <div style={{ height: '500px', width: '100%', margin: '0 auto' }}>
+      <Carousel
+        slides={cards}
+        goToSlide={goToSlide}
+        offsetRadius={offsetRadius}
+        showNavigation={showArrows}
+        animationConfig={config.gentle}
+      />
+    </div>
+  )
+}
+
+export default Carousel3D
